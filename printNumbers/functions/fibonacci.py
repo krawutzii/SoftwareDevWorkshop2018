893e9127cb0a3e3c8baac56f8fbd5315d0458949
krawutzii <vanberg90@gmail.com>
2018-11-19 14:06:26 +0100

    test

--- printNumbers/functions/fibonacci.py
+++ printNumbers/functions/fibonacci.py
@@ -19,6 +19,8 @@
 # You should have received a copy of the GNU General Public License
 # along with printNumbers.  If not, see <http://www.gnu.org/licenses/>.
 
+print('TEST')
+
 def FibonacciRecursion(n):
     '''
     Helper function.
