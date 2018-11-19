1e75080bb5d17515769fa729c66eb34dcdf58617
krawutzii <45167572+krawutzii@users.noreply.github.com>
2018-11-19 14:51:04 +0100

    Update factorial.py

--- printNumbers/functions/factorial.py
+++ printNumbers/functions/factorial.py
@@ -18,7 +18,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with printNumbers.  If not, see <http://www.gnu.org/licenses/>.
-
+print('bla')
 import math
 
 def Factorial(n):
